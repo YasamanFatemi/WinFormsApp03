3b219e74ef04ba1aa5056087582a4fd8c5e5f058
YasamanFatemi <yfatemi49@gmail.com>
2023-11-13 23:22:40 +0330

    up

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form4.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string name = textBox1.Text;
+                string id = textBox2.Text;
+                string price = textBox3.Text;
+                string line = name + "," + id + "," + price + "\n";
+                File.AppendAllText("kala.csv", line);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form5.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void Form5_Load(object sender, EventArgs e)
+        {
+            string[] lines = File.ReadAllLines("kala.csv");
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                listBox1.Items.Add(lines[i]);
+            }
+        }
+    }
+}
